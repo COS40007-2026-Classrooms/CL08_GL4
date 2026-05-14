f0fb438a418a540047ab1f0bafc3d761609795d7
Alex Phan <105334704@student.swin.edu.au>
2026-05-14 14:31:21 +1000

    Model comparison logic, if svm acc higher choose svm and vice versa

--- src/model.py
+++ src/model.py
@@ -32,3 +32,28 @@ svm_f1 = f1_score(y_test, svm_pred, average='weighted')
 print(f"SVM Accuracy: {svm_acc:.2f}")
 print(f"SVM F1-score: {svm_f1:.2f}")
 
+#Decision Tree
+print('\n Training Decision Tree Model')
+
+dt_model = DecisionTreeClassifier(max_depth=10, random_state=42)
+dt_model.fit(X_train, y_train)
+
+dt_pred = dt_model.predict(X_test)
+
+dt_acc = accuracy_score(y_test, dt_pred)
+dt_f1 = f1_score(y_test, dt_pred, average="weighted")
+
+print(f"Decision Tree Accuracy: {dt_acc:.4f}")
+print(f"Decision Tree F1-score: {dt_f1:.4f}")
+
+#Selecting model
+print('\n Model comparison Process')
+
+if svm_acc >= dt_acc:
+    best_model = svm_model
+    best_name = "SVM"
+else:
+    best_model = dt_model
+    best_name = "Decision Tree"
+
+print(f"Best Model: {best_name}")
