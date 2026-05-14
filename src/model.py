c91b80e1997a7606b5ffafb335d9628a45b9b5f3
Alex Phan <105334704@student.swin.edu.au>
2026-05-14 14:20:19 +1000

    Code for SVM model training and generating statistic

--- src/model.py
+++ src/model.py
@@ -15,3 +15,20 @@ X_train = np.load("artifacts/data/X_train.npy")
 X_test = np.load("artifacts/data/X_test.npy")
 y_train = np.load("artifacts/data/y_train.npy")
 y_test = np.load("artifacts/data/y_test.npy")
+
+print('Data Loaded Succesfully')
+
+#SVM Model
+print('\n Training SVM Model')
+
+svm_model = SVC(kernel='rbf', C=1, gamma='scale')
+svm_model.fit(X_train, y_train)
+
+svm_pred = svm_model.predict(X_test)
+
+svm_acc = accuracy_score(y_test, svm_pred)
+svm_f1 = f1_score(y_test, svm_pred, average='weighted')
+
+print(f"SVM Accuracy: {svm_acc:.2f}")
+print(f"SVM F1-score: {svm_f1:.2f}")
+
