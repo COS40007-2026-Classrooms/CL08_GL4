6b56a279c8b07c0f2c74d8b3723f6ea626735003
Alex Phan <105334704@student.swin.edu.au>
2026-05-17 11:57:36 +1000

    Saving feature columns

--- src/model.py
+++ src/model.py
@@ -1,15 +1,26 @@
 import numpy as np
 import joblib
 import os
+import json
+from datetime import datetime
 
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.preprocessing import StandardScaler
 
 
 print("="*60)
 print("MODEL TRAINING - SVM vs Decision Tree")
 print("="*60)
+
+#Directories
+os.makedirs("artifacts/models", exist_ok=True)
+os.makedirs("artifacts/preprocessing", exist_ok=True)
+os.makedirs("artifacts/metadata", exist_ok=True)
+os.makedirs("artifacts/metrics", exist_ok=True)
+os.makedirs("artifacts/data", exist_ok=True)
+
 
 #Data Loading
 X_train = np.load("artifacts/data/X_train.npy")
@@ -18,6 +29,22 @@ y_train = np.load("artifacts/data/y_train.npy")
 y_test = np.load("artifacts/data/y_test.npy")
 
 print('Data Loaded Succesfully')
+
+
+# ────────────────────────────────────
+# Feature Columns Metadata
+# ────────────────────────────────────
+#Saving feature columns
+num_features = X_train.shape[1]
+feature_columns = [f"feature_{i}" for i in range(num_features)]
+
+with open("artifacts/preprocessing/feature_columns.json", "w") as f:
+    json.dump(feature_columns, f, indent=4)
+print(f"\nSaved feature_columns.json ({num_features} features)")
+
+# ────────────────────────────────────
+# Target Column Info
+# ────────────────────────────────────
 
 #SVM Model
 print('\n Training SVM Model')
